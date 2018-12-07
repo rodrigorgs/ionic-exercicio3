0406fc2b8cdf7dfaf4a4ca27bc8d9cbec0c2d1f0
Rodrigo Rocha <rodrigorgs@gmail.com>
2018-12-06 21:36:08 -0300

    Adiciona especificação e testes

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -7,15 +7,124 @@ describe('App', () => {
     page = new Page();
   });
 
-  describe('default screen', () => {
+  describe('página inicial', () => {
     beforeEach(() => {
       page.navigateTo('/');
     });
 
-    it('should have a title saying Page One', () => {
-      page.getPageOneTitleText().then(title => {
-        expect(title).toEqual('Page One');
-      });
+    it('deve iniciar com os campos de texto vazios e o botão Remover desabilitado', () => {
+      page.getDescricao().then(texto => expect(texto).toEqual(''));
+      page.getPrioridade().then(texto => expect(texto).toEqual(''));
+      expect(page.buttonRemover().isEnabled()).toBeFalsy();
+    });
+
+    it('deve notificar se a prioridade for inválida', async () => {
+      await page.inserirTarefa("abc", 11);
+      let texto = await page.getMensagemToast();
+      expect(texto).toBe('A prioridade deve estar entre 1 e 10.');
+
+      let itens = await page.getItens();
+      expect(itens.length).toEqual(0);
+    });
+
+    it('deve notificar ao tentar inserir a mesma tarefa duas vezes', async () => {
+      await page.inserirTarefa("abc", 5);
+      await page.inserirTarefa("abc", 7);
+      let texto = await page.getMensagemToast();
+      expect(texto).toBe('Tarefa já cadastrada.');
+    });
+
+
+    it('deve inserir na lista uma tarefa válida', async () => {
+      await page.inserirTarefa('abc', 5);
+      let itens = await page.getItens();
+      expect(itens.length).toBe(1);
+      expect(itens[0]).toContain('abc');
+      expect(itens[0]).toContain('Prioridade: 5');
+    });
+
+    it('deve ativar o botão Remover ao adicionar um item', async () => {
+      await page.inserirTarefa('abc', 5);
+      expect(page.buttonRemover().isEnabled()).toBeTruthy();
+    });
+
+    it('deve desativar o botão Remover ao adicionar e remover um item', async () => {
+      await page.inserirTarefa('abc', 5);
+      await page.removerPrimeiro();
+      expect(page.buttonRemover().isEnabled()).toBeFalsy();
+    });
+
+    it('deve manter ordem ao inserir tarefas em ordem crescente de prioridade', async () => {
+      await page.inserirTarefa("a", 1);
+      await page.inserirTarefa("b", 6);
+      await page.inserirTarefa("c", 9);
+      let itens = await page.getItens();
+      expect(itens.length).toBe(3);
+      expect(itens[0]).toContain('a');
+      expect(itens[0]).toContain('Prioridade: 1');
+      expect(itens[1]).toContain('b');
+      expect(itens[1]).toContain('Prioridade: 6');
+      expect(itens[2]).toContain('c');
+      expect(itens[2]).toContain('Prioridade: 9');
+    });
+
+    it('deve inverter ordem ao inserir tarefas em ordem decrescente de prioridade', async () => {
+      await page.inserirTarefa("c", 9);
+      await page.inserirTarefa("b", 6);
+      await page.inserirTarefa("a", 1);
+      let itens = await page.getItens();
+      expect(itens.length).toBe(3);
+      expect(itens[0]).toContain('a');
+      expect(itens[0]).toContain('Prioridade: 1');
+      expect(itens[1]).toContain('b');
+      expect(itens[1]).toContain('Prioridade: 6');
+      expect(itens[2]).toContain('c');
+      expect(itens[2]).toContain('Prioridade: 9');
+    });
+
+    it('deve ordenar tarefas com prioridades iguais da mais antiga para a mais recente', async () => {
+      await page.inserirTarefa("c", 5);
+      await page.inserirTarefa("b", 6);
+      await page.inserirTarefa("a", 5);
+      let itens = await page.getItens();
+      expect(itens.length).toBe(3);
+      expect(itens[0]).toContain('c');
+      expect(itens[0]).toContain('Prioridade: 5');
+      expect(itens[1]).toContain('a');
+      expect(itens[1]).toContain('Prioridade: 5');
+      expect(itens[2]).toContain('b');
+      expect(itens[2]).toContain('Prioridade: 6');
+    });
+
+    it('deve remover segundo elemento da lista ao ser clicado', async () => {
+      await page.inserirTarefa("c", 9);
+      await page.inserirTarefa("b", 6);
+      await page.inserirTarefa("a", 1);
+      await page.inserirTarefa("z", 2);
+      await page.removerEnesimo(1);
+      let itens = await page.getItens();
+      expect(itens[0]).toContain('a');
+      expect(itens[0]).toContain('Prioridade: 1');
+      expect(itens[1]).toContain('b');
+      expect(itens[1]).toContain('Prioridade: 6');
+      expect(itens[2]).toContain('c');
+      expect(itens[2]).toContain('Prioridade: 9');
+    });
+
+    it('deve limpar os campos de texto se inserção for bem sucedida', async () => {
+      await page.inserirTarefa("abc", 9);
+      const desc = await page.getDescricao();
+      const pri = await page.getPrioridade();
+      expect(desc).toBe('');
+      expect(pri).toBe('');
+    });
+
+    it('não deve limpar os campos de texto se inserção não for bem sucedida', async () => {
+      await page.inserirTarefa("abc", 12);
+      const desc = await page.getDescricao();
+      const pri = await page.getPrioridade();
+      expect(desc).toBe('abc');
+      expect(pri).toBe('12');
     });
   })
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,4 +1,4 @@
-import { browser, by, element } from 'protractor';
+import { browser, by, element, protractor, ElementFinder } from 'protractor';
 
 export class Page {
 
@@ -6,11 +6,58 @@ export class Page {
     return browser.get(destination);
   }
 
-  getTitle() {
-    return browser.getTitle();
+  getDescricao() {
+    return this.editDescricao().getAttribute('value');
   }
 
-  getPageOneTitleText() {
-    return element(by.tagName('page-page1')).element(by.tagName('ion-title')).element(by.css('.toolbar-title')).getText();
+  getPrioridade() {
+    return this.editPrioridade().getAttribute('value');
+  }
+
+  async inserirTarefa(descricao, prioridade) {
+    await this.limpar(this.editDescricao());
+    await this.editDescricao().sendKeys(descricao);
+    await this.limpar(this.editPrioridade());
+    await this.editPrioridade().sendKeys(prioridade);
+    return this.buttonAdicionar().click();
+  }
+
+  getMensagemToast() {
+    return element(by.css('.toast-message')).getText();
+  }
+
+  getItens() {
+    return element.all(by.css('#listView .item'))
+      .map(elem => elem.getText())
+  }
+
+  removerPrimeiro() {
+    return this.buttonRemover().click();
+  }
+
+  removerEnesimo(n) {
+    return element.all(by.css('#listView .item')).get(n).click();
+  }
+
+  listView() {
+    return element(by.css('#listView'));
+  }
+  editDescricao() {
+    return element(by.css('#editDescricao input'));
+  }
+  editPrioridade() {
+    return element(by.css('#editPrioridade input'));
+  }
+  buttonAdicionar() {
+    return element(by.css('#buttonAdicionar'));
+  }
+  buttonRemover() {
+    return element(by.css('#buttonRemover'));
+  }
+
+  async limpar(elem: ElementFinder) {
+    let texto = await elem.getAttribute('value');
+    let array = Array(texto.length + 1).fill(protractor.Key.BACK_SPACE);
+    await elem.sendKeys(...array);
   }
 }
